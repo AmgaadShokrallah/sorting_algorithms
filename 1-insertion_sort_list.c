c1473a2cf3504f4df9f6c080c6162e2e3bc07bf5
AmgaadShokrallah <amgaad@example.com>
2023-12-15 21:12:53 -0800

    commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@
  */
 void swap_node(listint_t *node1, listint_t *node2)
 {
-	if (node2->next != NULL)
+	if (node2->next)
 		node2->next->prev = node1;
 	if (node1->prev)
 		node1->prev->next = node2;
@@ -29,11 +29,11 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *i, *t;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
 
 	i = (*list)->next;
-	while (i != NULL)
+	while (i)
 	{
 		t = i;
 		i = i->next;
