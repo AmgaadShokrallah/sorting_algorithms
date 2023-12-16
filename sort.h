112cc9ac04ce064669959b50577df2daccd56052
AmgaadShokrallah <amgaad@example.com>
2023-12-15 20:52:45 -0800

    commit

--- sort.h
+++ sort.h
@@ -1,10 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
-
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,11 +16,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Print functions */
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-void bubble_sort(int *array, size_t size);
+void swap_node(listint_t *node1, listint_t *node2)
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
