6b7948fdf68647122a6c5745559040fbd228fd02
AmgaadShokrallah <amgaad@example.com>
2023-12-17 23:38:50 -0800

    commit

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+*swap - swap 2 elements into an array
+*@array: array
+*@a: element1
+*@b: element2
+*/
+void swap(int *array, ssize_t a, ssize_t b)
+{
+	int tmp;
+
+	tmp = array[a];
+	array[a] = array[b];
+	array[b] = tmp;
+}
+/**
+ *lomuto_partition - lomuto partition sorting scheme implementation
+ *@array: array
+ *@low: first array element
+ *@high: last array element
+ *@size: size of array
+ *Return: return the position of the last element sorted
+ */
+int lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
+{
+	int pivot = array[high];
+	ssize_t i = low, j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (array[i] != array[j])
+			{
+				swap(array, i, j);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (array[i] != array[high])
+	{
+		swap(array, i, high);
+		print_array(array, size);
+	}
+	return (i);
+}
+/**
+ *quicksort - qucksort algorithm implementation
+ *@array: array
+ *@low: first array element
+ *@high: last array element
+ *@size: array of size
+ */
+void quicksort(int *array, ssize_t low, ssize_t high, int size)
+{
+	if (low < high)
+	{
+		size_t position = lomuto_partition(array, size, low, high);
+
+		quicksort(array, size, low, position - 1);
+		quicksort(array, size, position + 1, high);
+	}
+}
+/**
+ *quick_sort - prepare the terrain to quicksort algorithm
+ *@array: array
+ *@size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
+}
